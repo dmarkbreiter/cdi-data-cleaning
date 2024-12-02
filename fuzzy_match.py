a5aebf89eb909f1a2ec8c1f21171df12025462d9
Daniel Markbreiter <dmarkbreiter@nhm.org>
2024-12-02 10:58:25 -0800

    added join script

--- fuzzy_match.py
+++ fuzzy_match.py
@@ -1,74 +1,56 @@
 import pandas as pd
 import duckdb
-from time import time
-import polars as pl
-from rapidfuzz import fuzz
 
 df = pd.read_csv('data/vernaculars.csv')
 
-def match_polars(taxon:str):
-    vernaculars = pl.read_csv(
-        'data/vernaculars-cleaned.csv',
-        dtypes={"taxonID": pl.Float64}
-    )
-
-    # Compute the fuzzy match scores
-    vernaculars = vernaculars.with_columns(
-        vernaculars["canonicalName"].map(lambda x: fuzz.ratio(x, taxon)).alias("match_score")
-    )
-
-    # Get the row with the highest match score
-    highest_match = vernaculars.filter(pl.col("match_score") == vernaculars["match_score"].max())
-
-    return highest_match
-
 
 def match_duck_db(taxon:str, rank='species', threshold=0.95):
+    if not taxon or not rank: return ('', '')
+
     rank = rank.lower()
     taxon = taxon.replace("'", '').replace('"', '')
-    if rank == 'species':
 
+    if rank == 'species':
         try:
             genus = taxon.split(' ')[0]
             species = taxon.split(' ')[1]
         except ValueError:
             print("A species should be in the following format: '<genus> <species>'")
 
-        match = duckdb.query(
-            f"""
+        # Reassign rank to genus if sp. is listed as species
+        rank = 'genus' if species == 'sp.' or species == 'sp' else 'species'
+
+    match =(
+        f"""
             SELECT taxonID, genus, specificEpithet, vernacularName, canonicalName,
             (jaro_winkler_similarity('{genus}', genus) + jaro_winkler_similarity('{species}', specificEpithet)) / 2
             AS similarity 
             FROM df 
-            WHERE taxonRank = 'species'
+            WHERE taxonRank = 'species' AND similarity > 0.95
             ORDER BY similarity 
-            DESC LIMIT 1
-            """
-        ).df()
-    else:
-        match = duckdb.query(
-            f"""
+        """
+        if rank == 'species'
+        else f"""
             SELECT taxonID, canonicalName, vernacularName,
             jaro_winkler_similarity('{taxon}', canonicalName) 
             AS similarity,
             FROM df
-             WHERE taxonRank = '{rank}'
+            WHERE taxonRank = '{rank}'
             ORDER BY similarity 
-            DESC LIMIT 1
-            """
-        ).df()
+        """
+    )
 
-    if match.empty:
-        return ''
+    match = duckdb.query(match).df()
+
+    if match.empty: return ''
 
     match = match.sort_values('similarity', ascending=False).head(1).to_dict(orient="records")[0]
     match = match if match['similarity'] > threshold else ''
 
     return match
 
-match = match_duck_db('Thorius spilogasteri')
-
-print(match)
+match = match_duck_db('Crotalus mitchelli')
+#print(match)
 
 
 
--- main.py
+++ main.py
@@ -3,48 +3,72 @@ from fuzzy_match import match_duck_db
 from time import time
 from tqdm import tqdm
 
+# Read csvs as dataframes
 emu = pd.read_csv('data/cdi-biology-650k.csv')
 vernaculars = pd.read_csv('data/vernaculars-cleaned.csv')
 
-cols = [col for col in emu if col != 'taxon']
+# Clean
+emu.astype({'taxonIrn': 'Int64'})
+emu.fillna('', inplace=True)
+vernaculars.fillna('', inplace=True)
+
+# Remove unneeded columns
+cols = [col for col in emu if col not in ['taxon', 'taxonIrn']]
 emu_dropped = emu.drop(cols, axis=1)
 
+# Join based on canonicalName
 joined = pd.merge(emu_dropped, vernaculars, left_on='taxon', right_on='canonicalName')
 joined = joined.drop_duplicates(subset=['taxon'], keep='first')
-joined = joined.drop(['Unnamed: 0'], axis=1)
 
+# Create dictionary and taxa list
 vernacular_dict = joined.set_index('taxon').to_dict(orient="index")
-taxa = list(zip(emu['taxon'], emu['taxonRank']))[0:999]
+taxa = list(zip(emu['taxon'], emu['taxonRank'], emu['taxonIrn']))[999:1500]
 
 
-
-def get_vernacular(taxon:str, rank):
+# Main logic for assigning vernacular name
+def get_vernacular(taxon:str, rank, irn):
     match = vernacular_dict.get(taxon)
 
     if match:
-        return (match['vernacularName'], match['taxonID'])
+        return (match['vernacularName'], match['taxonID'], irn)
 
     else:
         match = match_duck_db(taxon, rank)
         if match:
             vernacular_dict[taxon] = {
+                'taxonIrn': irn,
                 'taxonID': match['taxonID'],
                 'vernacularName': match['vernacularName'],
                 'canonicalName': match['canonicalName'],
                 'similarity': match['similarity']
             }
-            return (match['vernacularName'], match['taxonID'])
-        return ('', '')
+            return (match['vernacularName'], match['taxonID'], irn)
+        return ('', '', '')
 
 
 
 start_time = time()
+
 results = [
-    get_vernacular(taxon[0], taxon[1])
+    get_vernacular(taxon[0], taxon[1], taxon[2])
     for taxon in tqdm(taxa, desc="Matching EMu taxa to vernacular names")
 ]
 
-results = pd.DataFrame(results, columns=['vernacularName', 'taxonID'])
+results = pd.DataFrame(results, columns=['vernacularName', 'taxonID', 'taxonIrn'])
 
 print(time() - start_time)
+
+# Save results
+emu['vernacularName'] = results['vernacularName'].to_list()
+emu['taxonID'] = results['taxonID'].to_list()
+emu['taxonIrn'] = results['taxonIrn'].to_list()
+
+emu.to_csv('data/emu-matched.csv')
+
+# Save dictionary
+vernacular_dict_df = pd.DataFrame.from_dict(vernacular_dict, orient='index')
+vernacular_dict_df.fillna('', inplace=True)
+vernacular_dict_df.to_csv('data/vernacular_dict.csv')
+
+
 print(results)
